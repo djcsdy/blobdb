b9ad2a4976473abc36e1ba453c47a0f6d1c2ce21
Daniel Cassidy <mail@danielcassidy.me.uk>
2021-07-26 13:36:59 +0100

    Add BlobId::name function.

--- src/lib/blob_id.rs
+++ src/lib/blob_id.rs
@@ -1,4 +1,17 @@
+use std::fmt::Write;
+use std::path::PathBuf;
+
 pub struct BlobId(pub [u8; 32]);
+
+impl BlobId {
+    pub fn name(&self) -> PathBuf {
+        let mut name = String::new();
+        for byte in self.0 {
+            write!(&mut name, "{:2x}", byte).unwrap();
+        }
+        name.into()
+    }
+}
 
 impl AsRef<[u8]> for BlobId {
     fn as_ref(&self) -> &[u8] {
