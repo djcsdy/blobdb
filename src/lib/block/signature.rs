850714578e637dec6ada93d715233dc058a11f37
Daniel Cassidy <mail@danielcassidy.me.uk>
2021-08-29 17:46:55 +0100

    BlockSignature: Implement constructor.

--- src/lib/block/signature.rs
+++ src/lib/block/signature.rs
@@ -8,6 +8,14 @@ pub enum BlockArity {
 }
 
 impl BlockSignature<'_> {
+    pub fn new(arity: BlockArity) -> BlockSignature<'static> {
+        match arity {
+            BlockArity::Invalid => BlockSignature(b"\0\0\0\0"),
+            BlockArity::OnePacket => BlockSignature(b"bDB\0"),
+            BlockArity::ManyPackets => BlockSignature(b"bD\xC2\0"),
+        }
+    }
+
     pub fn arity(&self) -> BlockArity {
         match self.0 {
             b"bDB\0" => BlockArity::OnePacket,
