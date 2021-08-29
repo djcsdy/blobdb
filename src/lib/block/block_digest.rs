24e3d8dabb9bb8396cea1273217c75a3c7d86d44
Daniel Cassidy <mail@danielcassidy.me.uk>
2021-08-29 19:48:24 +0100

    BlockDigest: Implement digest_block.

--- src/lib/block/block_digest.rs
+++ src/lib/block/block_digest.rs
@@ -1,2 +1,14 @@
+use crate::lib::block::Block;
+use sha2::{Digest, Sha256};
+
 #[derive(Copy, Clone, Eq, PartialEq)]
 pub struct BlockDigest<'a>(pub &'a [u8; 32]);
+
+impl BlockDigest<'_> {
+    pub fn digest_block(block: &Block) -> BlockDigest {
+        let mut digest = Sha256::new();
+        digest.update(&block.db_id());
+        digest.update(&block.0[52..]);
+        BlockDigest(&digest.finalize().into())
+    }
+}
