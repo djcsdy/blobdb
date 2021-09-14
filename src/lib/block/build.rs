480267327662229e044113880a09ece6da8a8516
Daniel Cassidy <mail@danielcassidy.me.uk>
2021-09-14 19:41:59 +0100

    Partially implement build_write_block function.

--- src/lib/block/build.rs
+++ src/lib/block/build.rs
@@ -1,37 +1,84 @@
-use crate::lib::block::block::{MANY_PACKETS_MAX_SIZE, PACKET_COUNT_SIZE};
+use crate::lib::block::block::MANY_PACKETS_MAX_SIZE;
 use crate::lib::block::block_digest::BLOCK_DIGEST_SIZE;
-use crate::lib::block::build::BuildWriteBlockResult::MoreBlocksRequired;
-use crate::lib::block::packet_provider::PacketProvider;
-use crate::lib::block::signature::BLOCK_SIGNATURE_SIZE;
+use crate::lib::block::packet_provider::{LargePacketResult, PacketProvider, PacketResult};
+use crate::lib::block::signature::{BlockArity, BlockSignature};
+use crate::lib::block::ONE_PACKET_MAX_SIZE;
 use crate::lib::db::DbId;
-use std::io::{Result, Seek, Write};
+use byteorder::WriteBytesExt;
+use std::io::{BufWriter, Result, Seek, Write};
 
-pub fn build_write_block<W>(
-    mut writer: W,
+pub fn build_write_block<W: Write + Seek>(
+    writer: &mut BufWriter<W>,
     db_id: &DbId,
-    packet_provider: Box<dyn PacketProvider<W>>,
-) -> Result<BuildWriteBlockResult>
-where
-    W: Write,
-    W: Seek,
-{
-    let signature_stream_position = writer.stream_position()?;
-    writer.write_all(&[0; BLOCK_SIGNATURE_SIZE])?;
+    packet_provider: &mut dyn PacketProvider<&mut BufWriter<W>>,
+) -> Result<BuildWriteBlockResult> {
+    let write_large_packet = match packet_provider.next_large_packet()? {
+        LargePacketResult::LargePacket(writer) => Some(writer),
+        LargePacketResult::TooSmall => None,
+        LargePacketResult::End => return Ok(BuildWriteBlockResult::Done),
+    };
+
+    let arity = match write_large_packet {
+        None => BlockArity::ManyPackets,
+        Some(_) => BlockArity::OnePacket,
+    };
+
+    writer.write_all(BlockSignature::new(arity).as_ref())?;
     writer.write_all(db_id.as_ref())?;
     let digest_stream_position = writer.stream_position()?;
     writer.write_all(&[0; BLOCK_DIGEST_SIZE])?;
-    let packet_count_stream_position = writer.stream_position()?;
-    let one_packet_stream_position = packet_count_stream_position;
-    let many_packets_stream_position = packet_count_stream_position + PACKET_COUNT_SIZE as u64;
 
-    let mut packets_count: u8 = 0;
-    let mut bytes_remaining = MANY_PACKETS_MAX_SIZE;
-
-    while bytes_remaining > 0 && packets_count < u8::MAX {
-        todo!();
+    let packet_count_stream_position = match write_large_packet {
+        None => Some(writer.stream_position()?),
+        Some(_) => None,
+    };
+    if write_large_packet.is_none() {
+        writer.write_u8(0)?;
     }
 
-    Ok(MoreBlocksRequired)
+    let packets_start_stream_position = writer.stream_position()?;
+
+    match write_large_packet {
+        None => {
+            let mut packets_count: u8 = 0;
+            let mut bytes_remaining = MANY_PACKETS_MAX_SIZE;
+
+            while bytes_remaining > 0 && packets_count < u8::MAX {
+                let packet_start_stream_position = writer.stream_position()?;
+                match packet_provider.next_packet(bytes_remaining)? {
+                    PacketResult::Packet(write_packet) => {
+                        write_packet(writer)?;
+                        let packet_size = writer.stream_position()? - packet_start_stream_position;
+                        if packet_size > bytes_remaining as u64 {
+                            panic!()
+                        }
+                        bytes_remaining -= packet_size as usize;
+                        packets_count += 1;
+                    }
+                    PacketResult::TooBig => {
+                        writer.write_all(vec![0 as u8; bytes_remaining].as_slice())?;
+                        return Ok(BuildWriteBlockResult::MoreBlocksRequired);
+                    }
+                    PacketResult::End => {
+                        writer.write_all(vec![0 as u8; bytes_remaining].as_slice())?;
+                        return Ok(BuildWriteBlockResult::Done);
+                    }
+                }
+            }
+
+            writer.write_all(vec![0 as u8; bytes_remaining].as_slice())?;
+        }
+        Some(write_packet) => {
+            write_packet(writer)?;
+            if writer.stream_position()? - packets_start_stream_position
+                != ONE_PACKET_MAX_SIZE as u64
+            {
+                panic!()
+            }
+        }
+    }
+
+    Ok(BuildWriteBlockResult::MoreBlocksRequired)
 }
 
 pub enum BuildWriteBlockResult {
--- src/lib/block/packet_provider.rs
+++ src/lib/block/packet_provider.rs
@@ -1,15 +1,24 @@
 use std::io::{Result, Seek, Write};
 
-pub trait PacketProvider<W>
-where
-    W: Write,
-    W: Seek,
-{
-    fn write_next_packet(&mut self, max_size: usize) -> Result<PacketProviderResult>;
+pub trait PacketProvider<W: Write + Seek> {
+    fn next_large_packet(&mut self) -> Result<LargePacketResult>;
+    fn next_packet(&mut self, max_size: usize) -> Result<PacketResult>;
 }
 
-pub enum PacketProviderResult {
-    Next,
+pub enum LargePacketResult {
+    LargePacket(WritePacket),
+    TooSmall,
+    End,
+}
+
+pub enum PacketResult {
+    Packet(WritePacket),
     TooBig,
     End,
 }
+
+pub type WritePacket = Box<dyn FnOnce(&mut dyn WriteSeek) -> Result<()>>;
+
+pub trait WriteSeek: Write + Seek {}
+
+impl<W: Write + Seek> WriteSeek for W {}
