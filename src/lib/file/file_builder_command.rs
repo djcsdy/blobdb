a7516a299b75aad97cb9c6a265666d819423ce60
Daniel Cassidy <mail@danielcassidy.me.uk>
2021-10-09 13:57:00 +0100

    Delete FileBuilderCommand.
    This is obsolete.

--- src/lib/file/file_builder_command.rs
+++
@@ -1,7 +0,0 @@
-use crate::lib::blob::BlobId;
-use crate::lib::packet::BlobDataPacket;
-
-pub enum FileBuilderCommand {
-    BlobData(BlobDataPacket),
-    BlobId(BlobId),
-}
--- src/lib/file/mod.rs
+++ src/lib/file/mod.rs
@@ -1,5 +1,2 @@
-mod file_builder_command;
 mod file_id;
 pub mod path;
-
-pub use file_builder_command::FileBuilderCommand;
