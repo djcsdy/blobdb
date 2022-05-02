30d75c7619b2a875954c2e7696121e6f6125189b
Daniel Cassidy <mail@danielcassidy.me.uk>
2022-05-02 18:38:08 +0100

    CLI: Print Blob ID after adding a Blob.

--- src/cli/add.rs
+++ src/cli/add.rs
@@ -6,5 +6,6 @@ use std::path::Path;
 pub fn add(options: AddOptions) -> () {
     let db = Db::open(Path::new(".")).unwrap();
     let mut file = File::open(&options.path).unwrap();
-    db.import_blob(&mut file).unwrap();
+    let blob_id = db.import_blob(&mut file).unwrap();
+    println!("{}", &blob_id);
 }
