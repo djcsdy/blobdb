113b684ee728a58d938ba38e4a51d5fd140d20a0
Daniel Cassidy <mail@danielcassidy.me.uk>
2022-05-02 18:32:10 +0100

    Add BlobId::parse() function.

--- src/lib/blob/blob_id.rs
+++ src/lib/blob/blob_id.rs
@@ -1,3 +1,5 @@
+use hex::FromHexError;
+
 pub const BLOB_ID_SIZE: usize = 32;
 
 #[derive(Copy, Clone, Eq, PartialEq)]
@@ -6,6 +8,12 @@ pub struct BlobId(pub [u8; BLOB_ID_SIZE]);
 impl BlobId {
     pub fn anonymous() -> BlobId {
         BlobId([0; BLOB_ID_SIZE])
+    }
+
+    pub fn parse<T: AsRef<[u8]>>(text: T) -> Result<BlobId, FromHexError> {
+        let mut buf = [0; BLOB_ID_SIZE];
+        hex::decode_to_slice(text, &mut buf)?;
+        Ok(BlobId(buf))
     }
 
     pub fn to_string(&self) -> String {
