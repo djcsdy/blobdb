d8933becc641a2cf982cd8b13776b5743e0567c5
Daniel Cassidy <mail@danielcassidy.me.uk>
2023-07-21 06:20:54 +0100

    Implement AsRef<[u8]> for KeyTablePacket.

--- src/lib/packet/key_table.rs
+++ src/lib/packet/key_table.rs
@@ -1,3 +1,9 @@
 use crate::lib::packet::RawPacket;
 
 pub struct KeyTablePacket(pub(super) RawPacket);
+
+impl AsRef<[u8]> for KeyTablePacket {
+    fn as_ref(&self) -> &[u8] {
+        self.0.as_ref()
+    }
+}
