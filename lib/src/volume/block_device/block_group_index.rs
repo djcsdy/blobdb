43c5b2f28b113feed0b8b62c5b96a50a9f9b1b15
Daniel Cassidy <mail@danielcassidy.me.uk>
2025-05-29 00:05:38 +0100

    Reimplement BlockGroupIndex using derive_more.

--- lib/src/volume/block_device/block_group_index.rs
+++ lib/src/volume/block_device/block_group_index.rs
@@ -1,44 +1,23 @@
-use std::ops::{Add, AddAssign, Sub, SubAssign};
+use derive_more::{Add, AddAssign, Deref, DerefMut, Display, From, Into, Sub, SubAssign};
 
-#[derive(Eq, PartialEq, Ord, PartialOrd, Clone, Copy, Hash, Debug)]
+#[derive(
+    Eq,
+    PartialEq,
+    Ord,
+    PartialOrd,
+    Clone,
+    Copy,
+    Hash,
+    Debug,
+    Add,
+    AddAssign,
+    Sub,
+    SubAssign,
+    From,
+    Into,
+    Display,
+    Deref,
+    DerefMut,
+)]
+#[display("Block Group {_0}")]
 pub struct BlockGroupIndex(pub u64);
-
-impl Add for BlockGroupIndex {
-    type Output = Self;
-
-    fn add(self, rhs: Self) -> Self::Output {
-        Self(self.0 + rhs.0)
-    }
-}
-
-impl AddAssign for BlockGroupIndex {
-    fn add_assign(&mut self, rhs: Self) {
-        self.0 += rhs.0;
-    }
-}
-
-impl Sub for BlockGroupIndex {
-    type Output = Self;
-
-    fn sub(self, rhs: Self) -> Self::Output {
-        Self(self.0 - rhs.0)
-    }
-}
-
-impl SubAssign for BlockGroupIndex {
-    fn sub_assign(&mut self, rhs: Self) {
-        self.0 -= rhs.0;
-    }
-}
-
-impl From<u64> for BlockGroupIndex {
-    fn from(value: u64) -> Self {
-        Self(value)
-    }
-}
-
-impl From<BlockGroupIndex> for u64 {
-    fn from(value: BlockGroupIndex) -> Self {
-        value.0
-    }
-}
